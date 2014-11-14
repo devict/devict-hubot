745c4330bc992fe7f1e8fcad85b3cc2749f46b90
Seth Etter <sethetter@gmail.com>
2014-11-14 11:33:04 -0600

    Seppuku!

---
+++ scripts/seppuku.js
@@ -0,0 +1,17 @@
+// Description:
+//   Bot has failed us
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   bot you (fail|failed) - bot commits seppuku.. HARRRGGHHNNNNN
+
+module.exports = function(robot) {
+  robot.respond(/you (fail|failed)/i, function(msg) {
+    msg.send('bot commits seppuku.. HARRRGGHHNNNNN');
+  });
+};
