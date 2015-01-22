a586bb251b0cfbed4aaf323abd951439570e9620
Seth Etter <sethetter@gmail.com>
2015-01-22 10:07:05 -0600

    Clean up column length in event script

--- scripts/events.js
+++ scripts/events.js
@@ -15,7 +15,10 @@
 
 module.exports = function(robot) {
   robot.respond(/events/i, function(msg) {
-    var url = 'http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=devict&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=73273692&sig=9cdd3af6b5a26eb664fe5abab6e5cf7bfaaf090e';
+    var url = 'http://api.meetup.com/2/events?status=upcoming' +
+      '&order=time&limited_events=False&group_urlname=devict' +
+      '&desc=false&offset=0&photo-host=public&format=json&page=20' +
+      '&fields=&sig_id=73273692&sig=9cdd3af6b5a26eb664fe5abab6e5cf7bfaaf090e';
 
     msg.http(url).get()(function(err, res, body) {
       if (err) {
