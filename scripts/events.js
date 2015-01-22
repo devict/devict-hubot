05c0c79d9e1031f566635c297d41fee3c0b4b236
Seth Etter <sethetter@gmail.com>
2015-01-22 10:07:56 -0600

    DIE SEMICOLONS!
    Also, forgot to include moment.. :)

--- scripts/events.js
+++ scripts/events.js
@@ -13,30 +13,32 @@
 // Commands:
 //   hubot events - Print a list of upcoming devICT events
 
+var moment = require('moment')
+
 module.exports = function(robot) {
   robot.respond(/events/i, function(msg) {
     var url = 'http://api.meetup.com/2/events?status=upcoming' +
       '&order=time&limited_events=False&group_urlname=devict' +
       '&desc=false&offset=0&photo-host=public&format=json&page=20' +
-      '&fields=&sig_id=73273692&sig=9cdd3af6b5a26eb664fe5abab6e5cf7bfaaf090e';
+      '&fields=&sig_id=73273692&sig=9cdd3af6b5a26eb664fe5abab6e5cf7bfaaf090e'
 
     msg.http(url).get()(function(err, res, body) {
       if (err) {
-        msg.send('Error making request: ' + err);
-        return;
+        msg.send('Error making request: ' + err)
+        return
       }
       if (200 != res.statusCode) {
-        msg.send('Request returned status code: ' + res.statusCode);
-        return;
+        msg.send('Request returned status code: ' + res.statusCode)
+        return
       }
 
-      var response = JSON.parse(body);
-      var eventStr = '';
+      var response = JSON.parse(body)
+      var eventStr = ''
       response.results.forEach(function(event) {
-        var dateStr = moment(event.time).tz('America/Chicago').format('dddd, MMM D, YYYY @ h:mm a');
-        eventStr +=  dateStr + ' :: ' + event.name + ' @ ' + event.venue.name + '\n';
-      });
+        var dateStr = moment(event.time).tz('America/Chicago').format('dddd, MMM D, YYYY @ h:mm a')
+        eventStr +=  dateStr + ' :: ' + event.name + ' @ ' + event.venue.name + '\n'
+      })
       msg.send(eventStr.trim())
-    });
-  });
-};
+    })
+  })
+}
