1924f159b12a2f6d6a27391659e351cdc030b9b4
Jacob Walker <jacobwalker0814@gmail.com>
2016-06-29 12:10:22 -0500

    Add option to opt out of bot responses

--- scripts/response.js
+++ scripts/response.js
@@ -1,5 +1,5 @@
 // Description:
-//   Would you just look at that
+//   Some automatic responses to common / semi-common sayings.
 //
 // Dependencies:
 //   None
@@ -9,9 +9,28 @@
 //
 // Commands:
 //   look at (this|that|it) - "look at that" guy
+//   so good - bot agrees
+
+// optOutUsers is a list of users who do not find value in hubot's
+// helpful responses.
+var optOutUsers = [
+  'tyrosinase',
+];
+
+var shouldAbort = function(msg) {
+  return optOutUsers.indexOf(msg.message.user.name) > -1;
+};
 
 module.exports = function(robot) {
+  robot.hear(/\bso good\b/i, function(msg) {
+    if (shouldAbort(msg)) { return; }
+
+    msg.send('https://cldup.com/M7UCWvoSIq.gif');
+  });
+
   robot.hear(/look at (this|that|it)/i, function(msg) {
+    if (shouldAbort(msg)) { return; }
+
     var images = [
       'http://cl.ly/image/2S2Y3B36443T/Screen%20Shot%202014-09-12%20at%2010.01.24%20PM.png',
       'https://cldup.com/pI8HiONY_L.png',
