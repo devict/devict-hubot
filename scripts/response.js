6e81117778339613699446f52b2e3f85832dac71
hitjim <hitjim@gmail.com>
2016-10-29 13:39:59 -0500

    trying out emoji response for so-good and look-at-that

--- scripts/response.js
+++ scripts/response.js
@@ -25,27 +25,13 @@ module.exports = function(robot) {
   robot.hear(/\bso good\b/i, function(msg) {
     if (shouldAbort(msg)) { return; }
 
-    msg.send('https://cldup.com/M7UCWvoSIq.gif');
+    msg.send('+:so-good:');
   });
 
   robot.hear(/look at (this|that|it)/i, function(msg) {
     if (shouldAbort(msg)) { return; }
 
-    var images = [
-      'http://cl.ly/image/2S2Y3B36443T/Screen%20Shot%202014-09-12%20at%2010.01.24%20PM.png',
-      'https://cldup.com/pI8HiONY_L.png',
-      'https://cldup.com/0HtaPe3xLB.jpg',
-      'https://cldup.com/N6CwJUnci1.png',
-      'https://cldup.com/zEHrnpcqRm.jpg',
-      'https://cldup.com/sGL_oezYfu.png',
-      'https://cldup.com/RC1eGHtnR9.png',
-      'https://cldup.com/nzwLw1PMJm.png',
-      'https://cldup.com/krWEF4wyaa.png',
-      'https://cldup.com/XeM2T-lfKr.png',
-      'https://cldup.com/IUVog8P4AV.png'
-    ];
-
-    msg.send(images[Math.floor(Math.random() * images.length)]);
+    msg.send('+:look-at-that:');
   });
 
   //regex curtesy of Michael Neth -> (yo)?u([^a-zA-Z0-9]| a)?re? welcome
